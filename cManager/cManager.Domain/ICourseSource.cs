5a6a9669fe514b496774dd877714ab6aafb5305a
mrjax <quesomonos@gmail.com>
2014-12-11 08:42:47 -0600

    VS Project Added

---
+++ cManager/cManager.Domain/ICourseSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cManager.Domain
+{
+    public interface ICourseSource
+    {
+        IQueryable<Course> Courses { get; }
+        IQueryable<Major> Majors
+        { get; }
+    }
+}
---
+++ cManager/cManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="cManager.Web.MvcApplication" Language="C#" %>
